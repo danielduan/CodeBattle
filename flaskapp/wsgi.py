01a12560f048e3b5492f85e816877c9704eb5080
Chau Tran <chau_tran@brown.edu>
2013-09-13 23:54:36 -0700

    Added support for multiple questions. Send only 1 request for each submit

--- flaskapp/wsgi.py
+++ flaskapp/wsgi.py
@@ -35,74 +35,73 @@ def get_test_cases(question):
   }
   return question_dict[question]
 
-@app.route('/run_tests', methods = ['GET'])
+@app.route('/run_tests', methods = ['POST'])
 def run_tests():
   callback = str(request.args['callback'])
   player = str(request.args['player'])
   code = str(request.args['code'])
-  question = str(request.args['question'])
+  questions = json.loads(str(request.args['questions']))
+  print 'printing'
+  print len(questions)
   lang = SYNTAX_TO_CODEPAD[request.args['lang']]
 
-  results = get_results(code, lang, question)
-  console_output = ''
-  response_dict = {}
-  for i, result in enumerate(results):
-    if str(result[0]) == 'False' and 'error' in str(result[1]).lower():
-      # response_dict[i] = str(result[1])
-      response_dict[i] = 'ERROR'
-    elif str(result[0]) == 'True':
-      response_dict[i] = 'PASS'
+  results = get_results(code, lang, questions)
+  
+  return callback + '(' + str(results) + ');'
+
+def get_results(code, lang, questions):
+  full_code = parse_code(code, lang, questions)
+  print full_code
+  codepad_url = 'http://codepad.org'
+  query_params = {
+    'code'    : full_code,
+    'lang'    : lang,
+    'private' : True,
+    'run'     : True
+  }
+  r = requests.post(codepad_url, query_params)
+  soup = BeautifulSoup(r.text)
+  output = soup('pre')[-1].get_text().strip()
+  return analyze_results(output, questions)
+
+def analyze_results(output, questions):
+  actual_outputs = [output for output in output.split('\n') if output!='']
+  expected_outputs = [output for question in questions for output in get_test_cases(question).values()]
+  print actual_outputs
+  print expected_outputs
+  results = {}
+  i=0
+  for actual, expected in zip(actual_outputs, expected_outputs):
+    if (actual == expected):
+      results[i] = 'PASS'
     else:
-      response_dict[i] = 'FAIL'
-  return callback + '(' + str(response_dict) + ');'
-
-def get_results(code, lang, question):
-  results = []
-  test_cases = get_test_cases(question)
-  for test_case in test_cases:
-    full_code = get_full_code(code, lang, question, test_case)
-    print full_code
-    codepad_url = 'http://codepad.org'
-    query_params = {
-      'code'    : full_code,
-      'lang'    : lang,
-      'private' : True,
-      'run'     : True
-    }
-    r = requests.post(codepad_url, query_params)
-    soup = BeautifulSoup(r.text)
-    output = soup('pre')[-1].get_text().strip()
-    # output = str(int(test_case) * 2)
-    results.append((output == test_cases[test_case], output))
-
+      results[i] = 'FAIL'
+    i = i + 1
   return results
 
-def get_full_code(code, lang, question, test_case):
-    function_call = question + '(' + test_case + ')'
-
-    print_statement = '\n'
-    if lang == 'C' or lang == 'C++':
-      print_statement += " \
-              int main() { \
-                  printf('%c\n'," + function_call + "); \
-              }"
-    elif lang == 'Python' or lang == 'Ruby':
-      print_statement += "print " + function_call
-    elif lang == 'PHP':
-      print_statement += "printf('%s\n'," + function_call + ");"
-
-    if lang == 'PHP':
-      opening_index = code.find('<?php')
-      if opening_index == -1:
-        code = '<?php\n' + code
-      close_index = code.find('?>')
-      if close_index != -1:
-        code = code[0:close_index]
-      code += print_statement + '?>'
-
-    else:
-      code += print_statement
-
+def parse_code(code, lang, questions):
+  for question in questions:
+    test_cases = get_test_cases(question)
+    for test_case in test_cases:
+      code += get_print_statement(code, lang, question, test_case)
+  if (lang == 'PHP'):
+    return '<?php ' + code + ' ?>'
+  else:
     return code
+
+def get_print_statement(code, lang, question, test_case):
+  function_call = question + '(' + test_case + ')'
+  print_statement = '\n'
+  if lang == 'C' or lang == 'C++':
+    print_statement += " \
+        int main() { \
+          printf('%s\n'," + function_call + "); \
+        }"
+  elif lang == 'Python' or lang == 'Ruby':
+    print_statement += "print " + function_call
+  elif lang == 'PHP':
+    print_statement += "printf('%s\n'," + function_call + ");"
+  return print_statement
+
 if __name__ == '__main__':
   app.run(debug=True)
