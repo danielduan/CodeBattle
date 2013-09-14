8e8e58e5fa94649ad2cdbc238d7bc91e6b847088
shazeline <shazeline@gmail.com>
2013-09-14 11:14:34 -0700

    add some questions.

--- js/questions.js
+++ js/questions.js
@@ -27,12 +27,30 @@ var problems = {
         "python": "def sumlist(x):\n return -1",
         "ruby": "def sumlist(x)\n return -1\nend",
         "php": "function sumlist($x)\n{\n return -1;\n}",
-        "comments": "Sumlist\n=======\nGiven an string of space separated integers, add them.\n\nINPUT\n\"1 2 3 4\"\n\nOUTPUT\n10"
+        "comments": "Sumlist\n=======\nGiven an string of space delimited integers, add them.\n\nINPUT\n\"1 2 3 4\"\n\nOUTPUT\n10"
     },
     "anagrams": {
         "python": "def anagrams(x):\n return \"\"",
         "ruby": "def anagrams(x)\n return \"\"\nend",
         "php": "function anagrams($x)\n{\n return \"\";\n}",
-        "comments": "Anagrams\n=======\nGiven an string of space separated strings, return a list of the counts\nof each unique group of anagrams found within the list.\n\nINPUT\n\"bat tab tub lol but\"\n\nOUTPUT\n\"2 2 1\""
+        "comments": "Anagrams\n=======\nGiven an string of space delimited strings, return a list of the counts\nof each unique group of anagrams found within the list.\n\nINPUT\n\"bat tab tub lol but\"\n\nOUTPUT\n\"2 2 1\""
+    },
+    "string_match": {
+        "python": "def string_match(s):\n return \"\"",
+        "ruby": "def string_match(s)\n return \"\"\nend",
+        "php": "function string_match($s)\n{\n return \"\";\n}",
+        "comments": "String Match\n=======\nGiven a string with two space delimited strings, a and b, return the number of the positions \nwhere they contain the same length 2 substring.\n\nINPUT\n\"'xxcaazz xxbaaz'\"\n\nOUTPUT\n\"3\""
+    },
+    "median": {
+        "python": "def median(s):\n return \"\"",
+        "ruby": "def median(s)\n return \"\"\nend",
+        "php": "function median($s)\n{\n return \"\";\n}",
+        "comments": "Median\n=======\nGiven an string of an odd number of space delimited integers, find the median.\n\nINPUT\n\"'9 4 12 5 0'\"\n\nOUTPUT\n\"5\""
+    },
+    "thirty_times" : {
+        "python": "def thirty_times(s):\n return \"\"",
+        "ruby": "def thirty_times(s)\n return \"\"\nend",
+        "php": "function thirty_times($s)\n{\n return \"\";\n}",
+        "comments": "Thirty Times\n=======\nGiven an string s, return 30 copies of it stringed together.\n\nINPUT\n\"'b'\"\n\nOUTPUT\n\"'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'\""
     }
 }
